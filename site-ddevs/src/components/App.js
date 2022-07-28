acf4446f27563e6b7bf9344cdd840d672aa8e807
xHerion <99676970+xHerion@users.noreply.github.com>
2022-07-27 23:47:39 -0300

    Fontawesome_icons

--- site-ddevs/src/components/App.js
+++ site-ddevs/src/components/App.js
@@ -1,19 +1,25 @@
 import '../CSS/App.css';
 import React from 'react';
 import Logo from '../../src/logo/logo_transparent.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee  } from '@fortawesome/free-solid-svg-icons'
+
 
 
 function App() {
   return (
-    <div >
+    <div>
       <div className='Box'>
         <div className='navbar'>
           <p className='navbar-options'>Inicio</p>
           <p className='navbar-options'>Portifólio</p>
-          <p className='navbar-options'>Contato</p>
+          <p className='navbar-options'>Contato</p>      
+          <div>
+          </div>
         </div>
-      <img src={Logo} className='logo-banner' alt='logo-ddevs'/>
+        <img src={Logo} className='logo-banner' alt='logo-ddevs' />
       </div>
+      <FontAwesomeIcon icon={faCoffee }></FontAwesomeIcon>
     </div>
   );
 }
