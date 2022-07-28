152744f4bf55c5eb382045f4329f7412d8017772
xHerion <99676970+xHerion@users.noreply.github.com>
2022-07-28 05:21:03 -0300

    layout2

--- site-ddevs/src/components/App.js
+++ site-ddevs/src/components/App.js
@@ -2,26 +2,33 @@ import '../CSS/App.css';
 import React from 'react';
 import Logo from '../../src/logo/logo_transparent.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass  } from '@fortawesome/free-solid-svg-icons'
-
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import banner1 from '../banner1.png'
 
 
 function App() {
   return (
-    <div>
+    <div className='body'>
       <div className='Box'>
         <div className='navbar'>
           <p className='navbar-options'>Inicio</p>
           <p className='navbar-options'>Portifólio</p>
-               
+
           <div className='SearchPosition'>
-          <p className='contato-position'>Contato</p> 
-          <FontAwesomeIcon className='Search' icon={faMagnifyingGlass } ></FontAwesomeIcon>
+            <p className='contato-position'>Contato</p>
+            <FontAwesomeIcon className='Search' icon={faMagnifyingGlass} ></FontAwesomeIcon>
           </div>
         </div>
         <img src={Logo} className='logo-banner' alt='logo-ddevs' />
       </div>
-     
+    { 
+    /* 
+   
+    Inicio da estrutura para o carrossel 
+    <------------------------------>
+    */ 
+    }
+
     </div>
   );
 }
