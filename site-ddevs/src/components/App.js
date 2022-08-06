f374c9fe03a51573136f57cd1ae91e1102ae2d4e
xHerion <99676970+xHerion@users.noreply.github.com>
2022-08-06 06:26:40 -0300

    layout2
    Inserindo caixa Sobre na pagina inicial.

--- site-ddevs/src/components/App.js
+++ site-ddevs/src/components/App.js
@@ -2,8 +2,12 @@ import '../CSS/App.css';
 import React from 'react';
 import Logo from '../../src/logo/logo_transparent.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import banner1 from '../banner1.png'
+import {
+  faMagnifyingGlass,
+  faCaretDown
+} from '@fortawesome/free-solid-svg-icons'
+
+
 
 
 function App() {
@@ -13,22 +17,39 @@ function App() {
         <div className='navbar'>
           <p className='navbar-options'>Inicio</p>
           <p className='navbar-options'>Portifólio</p>
-
           <div className='SearchPosition'>
             <p className='contato-position'>Contato</p>
             <FontAwesomeIcon className='Search' icon={faMagnifyingGlass} ></FontAwesomeIcon>
           </div>
         </div>
-        <img src={Logo} className='logo-banner' alt='logo-ddevs' />
-      </div>
-    { 
-    /* 
-   
-    Inicio da estrutura para o carrossel 
-    <------------------------------>
-    */ 
-    }
 
+        <div className='caixaBanner'>
+          <img src={Logo} className='logo-banner' alt='logo-ddevs' />
+        </div>
+
+      </div>
+
+      <div className='Caixasobre'>
+      <div className='caixaTituloSobre'>
+        <h1 className='tituloSobre'><FontAwesomeIcon id='caretIcon' icon={faCaretDown} />Sobre nos</h1>
+      </div>
+        <div className='caixaTexto'>
+          <p className='sobre'>
+            Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+
+        </div>
+        <FontAwesomeIcon className='SetaIcon' icon={faCaretDown} />
+      </div>
     </div>
   );
 }
